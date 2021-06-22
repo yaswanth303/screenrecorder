cd6daeba6cff0825d4a21da450e4c5f95b43a15c
yaswanth303 <yaswanthm03@gmail.com>
2021-06-22 18:27:53 +0530

    Add files via upload

---
+++ screenrecorder.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 11:01:08 2021
+
+@author: 2304349
+"""
+
+
+import streamlit as st
+import base64
+from io import BytesIO
+
+
+st.image('uni2.jpg', width=None, use_column_width=True)
+
+st.markdown("<h1 style='text-align: center; color: black;'>Screen Recorder</h1>", unsafe_allow_html=True)
